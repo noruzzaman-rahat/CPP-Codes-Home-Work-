ab7f58ada0249964374602113f5f9f0c6589c2ba
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-03 18:44:23 +0600

    Add files via upload

---
+++ even_odd-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void OE()
+{
+    int x;
+    cin>>x;
+    if(x%2==0)
+    {
+        cout<<"Even"<<endl;
+
+    }
+    else
+    {
+        cout<<"Odd"<<endl;
+    }
+}
+int main()
+{
+    OE();
+    return 0;
+}
