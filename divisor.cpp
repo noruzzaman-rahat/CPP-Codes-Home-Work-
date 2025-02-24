23d762b37f8db5157cb0b4288ac320d174ddac2e
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-24 11:22:00 +0600

    Add files via upload

---
+++ divisor.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+class number
+{
+public:
+    void divisors()
+    {
+        int n;
+        cin>>n;
+        for(int i = 2; i <= n/2; i++){
+            if(n%i == 0){
+                cout<<i<<endl;
+            }
+        }
+    }
+};
+int main()
+{
+    number obj;
+    obj.divisors();
+}
